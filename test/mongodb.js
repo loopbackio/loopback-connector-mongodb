a712d98bc3d38366b1d30ada555a502c153ce1f4
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2012-12-15 00:59:47 +0400

    Query by _id in nested resource

--- test/mongodb.js
+++ test/mongodb.js
@@ -5,7 +5,26 @@ var jdb = require('jugglingdb'),
         url: 'mongodb://travis:test@localhost:27017/myapp'
     });
 
+var ObjectID = require('mongodb').ObjectID;
+
 schema.name = 'mongodb';
 
 test(module.exports, schema);
 
+test.it('hasMany should support additional conditions', function (test) {
+
+    Post = schema.models.Post;
+    User = schema.models.User;
+
+    User.create(function (e, u) {
+        u.posts.create({}, function (e, p) {
+            console.log(arguments);
+            u.posts({where: {_id: p.id}}, function (e, posts) {
+                console.log(arguments);
+                test.equal(posts.length, 1, 'There should be only 1 post.');
+                test.done();
+            });
+        });
+    });
+
+});
