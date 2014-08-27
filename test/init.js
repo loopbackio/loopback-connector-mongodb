8d86533abe266ada78a0cb84fcfece6f7f35ab0e
Raymond Feng <raymond@strongloop.com>
2014-08-27 11:05:17 -0700

    Add a test case for ping errors

--- test/init.js
+++ test/init.js
@@ -4,8 +4,8 @@ var DataSource = require('loopback-datasource-juggler').DataSource;
 
 var config = require('rc')('loopback', {test: {mongodb: {}}}).test.mongodb;
 
-global.getDataSource = global.getSchema = function () {
-  var db = new DataSource(require('../'), config);
+global.getDataSource = global.getSchema = function (customConfig) {
+  var db = new DataSource(require('../'), customConfig || config);
   db.log = function (a) {
     console.log(a);
   };
