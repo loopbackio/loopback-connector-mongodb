35c4299d9fbc4922e361eba19dfa3b77116b3dc1
Raymond Feng <raymond@strongloop.com>
2015-08-25 15:21:34 -0700

    Enhance coercion of ids with inq/nin operators
    Fix https://github.com/strongloop/loopback-connector-mongodb/issues/52

--- test/id.test.js
+++ test/id.test.js
@@ -16,7 +16,15 @@ describe('mongodb custom id name', function () {
       age: 30
     }, function(err, customer) {
       customer.seq.should.equal(1);
-      done(err, customer);
+      Customer.create({
+        seq: 2,
+        name: 'John2',
+        emails: ['john2@x.com', 'john2@y.com'],
+        age: 40
+      }, function(err, customer) {
+        customer.seq.should.equal(2);
+        done(err, customer);
+      });
     });
   });
 
@@ -26,6 +34,72 @@ describe('mongodb custom id name', function () {
       done(err, customer);
     });
   });
+
+  it('should allow inq with find', function(done) {
+    Customer.find({where: {seq: {inq: [1]}}}, function(err, customers) {
+      customers.length.should.equal(1);
+      customers[0].seq.should.equal(1);
+      done(err);
+    });
+  });
+
+});
+
+describe('mongodb string id', function () {
+  var Customer = ds.createModel('customer2', {seq: {type: String, id: true},
+    name: String, emails: [String], age: Number});
+  var customer1, customer2;
+
+  before(function(done) {
+    Customer.deleteAll(done);
+  });
+
+  it('should allow custom name for the id property for create', function(done) {
+    Customer.create({
+      seq: '1',
+      name: 'John1',
+      emails: ['john@x.com', 'john@y.com'],
+      age: 30
+    }, function(err, customer) {
+      customer.seq.should.equal('1');
+      customer1 = customer;
+      var customer2Id = new ds.ObjectID().toString();
+      Customer.create({
+        seq: customer2Id,
+        name: 'John2',
+        emails: ['john2@x.com', 'john2@y.com'],
+        age: 40
+      }, function(err, customer) {
+        customer2 = customer;
+        customer.seq.should.equal(customer2Id);
+        done(err, customer);
+      });
+    });
+  });
+
+  it('should allow custom name for the id property for findById', function(done) {
+    Customer.findById(1, function(err, customer) {
+      customer.seq.should.equal('1');
+      done(err, customer);
+    });
+  });
+
+  it('should allow inq with find', function(done) {
+    Customer.find({where: {seq: {inq: [1]}}}, function(err, customers) {
+      customers.length.should.equal(1);
+      customers[0].seq.should.equal('1');
+      done(err);
+    });
+  });
+
+  it('should allow inq with find', function(done) {
+    Customer.find({where: {seq: {inq: [customer2.seq]}}}, function(err, customers) {
+      customers.length.should.equal(1);
+      customers[0].seq.should.equal(customer2.seq);
+      done(err);
+    });
+  });
+
 });
 
 describe('mongodb default id type', function () {
