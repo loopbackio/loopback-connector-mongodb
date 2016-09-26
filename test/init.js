970bd1de2d407d6d94146e2048747e9f9fc62ccf
Nick Duffy <duffn@outlook.com>
2016-09-26 12:06:18 -0600

    Add connectorCapabilities global object

--- test/init.js
+++ test/init.js
@@ -31,4 +31,9 @@ global.getDataSource = global.getSchema = function(customConfig) {
   return db;
 };
 
+global.connectorCapabilities = {
+  ilike: false,
+  nilike: false,
+};
+
 global.sinon = require('sinon');
