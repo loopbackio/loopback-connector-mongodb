c475e72670bb2de7553b411f23d9c358a0b5871f
Louis Beullens <louis@beullens.com>
2019-10-25 16:42:41 +0200

    implicitNullType + tests

--- test/connector-functions.test.js
+++ test/connector-functions.test.js
@@ -31,3 +31,43 @@ describe('connector function - findById', function() {
     });
   });
 });
+
+describe('find (implicitNullType)', function() {
+  let db, TestAlias, sampleId;
+  let implicitNullType=false
+  beforeEach(function(done) {
+    db = global.getDataSource({
+      host: '127.0.0.1',
+      port: global.config.port,
+      implicitNullType,
+    });
+    implicitNullType = !implicitNullType
+    TestAlias = db.define('TestAlias', {foo: {type: String}});
+    db.automigrate(function(err) {
+      if (err) return done(err);
+      TestAlias.create({foo: 'foo'}, function(err, t) {
+        if (err) return done(err);
+        sampleId = t.id;
+        done();
+      });
+    });
+  });
+
+  it('find none by id: sampleId, deletedAt: null (implicitNullType=false)', function(done) {
+    db.connector.all('TestAlias', {where: {id: sampleId, deletedAt: null}}, {}, function(err, r) {
+      if (err) return done(err);
+      if (r.length) {
+        return done(new Error('all should not have found the TestAlias document'))
+      }
+      done();
+    });
+  });
+
+  it('find all by id: sampleId, deletedAt: null (implicitNullType=true)', function(done) {
+    db.connector.all('TestAlias', {where: {id: sampleId, deletedAt: null}}, {}, function(err, r) {
+      if (err) return done(err);
+      r[0].foo.should.equal('foo');
+      done();
+    });
+  });
+});
